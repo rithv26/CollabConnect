a8596962d2d3991f124b78084ccd7f76dc15a081
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2024-07-31 02:13:51 +0530

    env and loading

--- client/src/integrals/Home.jsx
+++ client/src/integrals/Home.jsx
@@ -3,9 +3,18 @@ import { useAuth } from "../components/AuthContext";
 import Navbar from "../components/Navbar";
 import Globe from "./Globe";
 import Map from "./Map";
+import { ColorRing } from "react-loader-spinner";
 
 function Home() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <ColorRing colors={['#000000', '#000000', '#000000', '#000000', '#000000']} width="60" height="60" color="#000000" />
+      </div>
+    );
+  }
 
   return (
     <>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
-import config from '../auth_config.json';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
-      domain={config.domain}
-      clientId={config.clientId}
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
       cacheLocation="localstorage"
     >
