d768a55cd372e3cef8302dcb3ae871ca596b6a6c
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2024-08-08 12:19:49 +0530

    modal style

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -17,7 +17,7 @@ const Modal = ({ isOpen, onClose, emailContent, onSendEmail, toWho, feedback=fal
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-transparent">
       <div
         className={`w-[450px] rounded-2xl p-4 ${
-          feedback ? "bg-black bg-opacity-80" : "bg-white bg-opacity-30"
+          feedback ? "bg-black bg-opacity-80" : "bg-black bg-opacity-80"
         }`}
       >
         <h1 className="mb-4 ml-2 font-Quicksand text-xl font-bold">
