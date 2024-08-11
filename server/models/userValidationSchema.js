5de6202e43f30228bd29fe533e53ef4409c55e6b
Rithvik Singh <rsingh26@terpmail.umd.edu>
2024-08-11 05:34:38 +0400

    zod validation schema + dependency

---
+++ server/models/userValidationSchema.js
@@ -0,0 +1,23 @@
+const z = require('zod');
+
+const userValidationSchema = z.object({
+  auth0Id: z.string(),
+  name: z.string().optional(),
+  email: z.string().email().optional().or(z.literal('')),
+  description: z.string().max(500).optional(),
+  profileCompleted: z.boolean().optional(),
+  isHacker: z.boolean().optional(),
+  isDeveloper: z.boolean().optional(),
+  isResearcher: z.boolean().optional(),
+  timezone: z.string().optional(),
+  location: z.object({
+    type: z.literal("Point").optional(),
+    coordinates: z.tuple([z.number(), z.number()]).optional()
+  }).optional(),
+  previousHackathons: z.array(z.string()).optional(),
+  devpostProfile: z.string().url().optional().or(z.literal('')),
+  researchProfile: z.string().url().optional().or(z.literal('')),
+  githubUsername: z.string().optional()
+});
+
+module.exports = userValidationSchema;
